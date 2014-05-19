63f878aa373a9ba57b6d103151d47cbf534cdb0b
szymonmat <bosik111@gmail.com>
2014-05-19 20:37:34 +0200

    Adding project

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+	<head>
+		<title>
+		1508 birthday celebration
+		</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+		 <script src="js/paralax.js"></script>
+	</head>
+
+	<body>
+		<div class='wrapper'>
+			<nav class="mainMenu">
+				<ul>
+					<li>Sign up</li>
+					<li>About the Event</li>
+					<li>Schedule</li>
+					<li>Guest Speakers</li>
+					<li>Find Us</li>
+				</ul>
+			</nav>
+				<div class="mainContent">
+					<article>
+sdfdsfsdf
+						
+					</article>
+				</div>
+			
+		</div>
+
+
+
+
+		<!-- SCRIPT BLOCK-->
+		<script type="text/javascript" src="script.js"></script>
+	</body>
+</html>
